e6b78a645fea4760792f8e82724fb0928db899ef
Cristiano Dias <cristianormdias@gmail.com>
2020-07-31 00:25:29 -0300

    Bumps gun version to 2.0-pre, which is compatible with cowlib 2.9

--- mix.exs
+++ mix.exs
@@ -86,7 +86,7 @@ defmodule Nostrum.Mixfile do
     [
       {:httpoison, "~> 1.7"},
       {:poison, "~> 3.0"},
-      {:gun, "~> 1.3"},
+      {:gun, git: "https://github.com/ninenines/gun.git", tag: "2.0.0-pre.2"},
       {:ex_doc, "~> 0.14", only: :dev},
       {:credo, "~> 0.4", only: [:dev, :test]},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
